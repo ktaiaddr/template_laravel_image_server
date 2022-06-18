5f8b0203632dd12cb758e7862fa5627138d0b0b3
ktai.addr <ktai.addr@gmail.com>
2022-06-18 10:40:46 +0900

    api修正

--- laravel9/routes/api.php
+++ laravel9/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\File\File;
+use Illuminate\Support\Facades\Storage;
+use Rorecek\Ulid\Facades\Ulid;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,25 +24,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/image', function (Request $request) {
 
+    // base64文字列（data:image/png;base64,iVBORw～～）を受取り
     $base64 = $request->input('base64');
+
+    // カンマで分割
     $base64_ex = explode(',',$base64);
+
+    // カンマ以降の文字列
     $fileData =  base64_decode($base64_ex[1]);
-    $tmpFilePath = sys_get_temp_dir() . '/' . Str::uuid()->toString();
+
+    $filename = Str::orderedUuid()->toString();
+
+    // 保存先パスを取得
+    $tmpFilePath = sys_get_temp_dir() . '/' . $filename.'.png';
+
     file_put_contents($tmpFilePath, $fileData);
 
-    $tmpFile = new File($tmpFilePath);
+    Storage::disk('sftp')->putFileAs( 'public', $tmpFilePath,$filename.'.png');
 
-    $filename = 'hoge_'.Str::uuid()->toString().'.png';
-    Storage::disk('sftp')->putFileAs( 'public', $tmpFile,$filename);
-    //dd(asset('storage/hoge.jpg'));
-    //Storage::putFileAs('', $tmpFile,'hoge.jpg');
     return response()->json([asset('api/image/'.$filename)]);
 
 });
 
 Route::get('/image/{filename}', function (Request $request, string $filename) {
 
-    $filedata = Storage::disk('sftp')->get('public/'.$filename);
+    $filedata = Storage::disk('sftp')->get('public/'.$filename.'.png');
+
     return response(($filedata))->header('content-Type', 'image/png');
 });
 
